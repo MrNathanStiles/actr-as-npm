1aaf25664b06999a560c2cebb6b8fc7880f2b069
Nathan Stiles <stiles.nathan@outlook.com>
2025-02-26 21:45:19 -0500

    surface nets

--- src/surface-nets.ts
+++ src/surface-nets.ts
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+import { actr_log } from "./log";
+
 /**
  * SurfaceNets in JavaScript ported to Assembly Script
  *
@@ -71,7 +73,7 @@ function precomputeEdgeTable(): void {
 }
 
 export class SurfaceNetGenerator {
-    
+
     public constructor() {
         precomputeEdgeTable();
     }
@@ -99,8 +101,8 @@ export class SurfaceNetGenerator {
 
 export class SurfaceNet {
     public constructor(
-        public vertices: StaticArray<StaticArray<f32>>,
-        public faces: StaticArray<StaticArray<i32>>,
+        public vertices: StaticArray<f32>,
+        public faces: StaticArray<i32>,
     ) { }
 }
 
@@ -120,8 +122,8 @@ export class SurfaceData {
             , R = new StaticArray<i32>(3)
             , grid = new StaticArray<f32>(8)
             , buf_no = 1;
-        const vertices: Array<StaticArray<f32>> = new Array();
-        const faces: Array<StaticArray<i32>> = new Array();
+        const vertices: Array<f32> = new Array();
+        const faces: Array<i32> = new Array();
         R[0] = 1;
         R[1] = dims[0] + 1;
         R[2] = (dims[0] + 1) * (dims[1] + 1);
@@ -129,10 +131,14 @@ export class SurfaceData {
         const buffer = (R[2] * 2 > 4096)
             ? new StaticArray<i32>(R[2] * 2)
             : new StaticArray<i32>(4096);
-    
+        actr_log(`face prediction ${R[2] * 2}`);
+        let pushCount: i32 = 0;
+        let maxx: f64 = 0;
+        let maxy: f64 = 0;
+        let maxz: f64 = 0;
         // March over the voxel grid
         for (x[2] = 0; x[2] < dims[2] - 1; ++x[2], n += dims[0], buf_no ^= 1, R[2] = -R[2]) {
-    
+
             // m is the pointer into the buffer we are going to use.  
             // This is slightly obtuse because javascript does not have good support for packed data structures, so we must use typed arrays :(
             // The contents of the buffer will be the indices of the vertices on the previous x/y slice of the volume
@@ -141,7 +147,7 @@ export class SurfaceData {
             let j: i32;
             for (x[1] = 0; x[1] < dims[1] - 1; ++x[1], ++n, m += 2)
                 for (x[0] = 0; x[0] < dims[0] - 1; ++x[0], ++n, ++m) {
-    
+
                     //Read in 8 field values around this vertex and store them in an array
                     //Also calculate 8-bit mask, like in marching cubes, so we can speed up sign checks later
                     var mask = 0, g = 0, idx = n;
@@ -152,31 +158,31 @@ export class SurfaceData {
                                 grid[g] = p;
                                 mask |= (p < 0) ? (1 << g) : 0;
                             }
-    
+
                     //Check for early termination if cell does not intersect boundary
                     if (mask === 0 || mask === 0xff) {
                         continue;
                     }
-    
+
                     //Sum up edge intersections
                     var edge_mask = edge_table[mask]
                         //, v = [0.0, 0.0, 0.0]
                         , e_count = 0;
-    
-                    const v: StaticArray<f32> = StaticArray.fromArray<f32>([0.0,0.0,0.0]);
-    
-    
+
+                    const v: StaticArray<f32> = StaticArray.fromArray<f32>([0.0, 0.0, 0.0]);
+
+
                     //For every edge of the cube...
                     for (i = 0; i < 12; ++i) {
-    
+
                         //Use edge mask to check if it is crossed
                         if (!(edge_mask & (1 << i))) {
                             continue;
                         }
-    
+
                         //If it did, increment number of edge crossings
                         ++e_count;
-    
+
                         //Now find the point of intersection
                         var e0 = cube_edges[i << 1]       //Unpack vertices
                             , e1 = cube_edges[(i << 1) + 1]
@@ -188,7 +194,7 @@ export class SurfaceData {
                         } else {
                             continue;
                         }
-    
+
                         //Interpolate vertices and add up intersections (this can be done without multiplying)
                         for (j = 0, k = 1; j < 3; ++j, k <<= 1) {
                             var a = e0 & k
@@ -200,47 +206,81 @@ export class SurfaceData {
                             }
                         }
                     }
-    
+
                     //Now we just average the edge intersections and add them to coordinate
                     var s: f32 = 1.0 / (f32)(e_count);
                     for (i = 0; i < 3; ++i) {
                         v[i] = (f32)(x[i]) + s * v[i];
                     }
-    
+
                     //Add vertex to buffer, store pointer to vertex index in buffer
-                    buffer[m] = vertices.length;
-                    vertices.push(v);
-    
+                    buffer[m] = pushCount++;
+                    maxx = Math.max(v[0], maxx);
+                    maxy = Math.max(v[0], maxy);
+                    maxz = Math.max(v[0], maxz);
+                    vertices.push(v[0]);
+                    vertices.push(v[1]);
+                    vertices.push(v[2]);
+
                     //Now we need to add faces together, to do this we just loop over 3 basis components
                     for (i = 0; i < 3; ++i) {
                         //The first three entries of the edge_mask count the crossings along the edge
                         if (!(edge_mask & (1 << i))) {
                             continue;
                         }
-    
+
                         // i = axes we are point along.  iu, iv = orthogonal axes
                         var iu = (i + 1) % 3
                             , iv = (i + 2) % 3;
-    
+
                         //If we are on a boundary, skip it
                         if (x[iu] === 0 || x[iv] === 0) {
                             continue;
                         }
-    
+
                         //Otherwise, look up adjacent edges in buffer
                         var du = R[iu]
                             , dv = R[iv];
-    
+
                         //Remember to flip orientation depending on the sign of the corner.
                         if (mask & 1) {
-                            faces.push(StaticArray.fromArray([buffer[m], buffer[m - du], buffer[m - du - dv], buffer[m - dv]]));
+                            // faces.push([buffer[m], buffer[m - du], buffer[m - du - dv], buffer[m - dv]]);
+                            faces.push(buffer[m]);
+                            faces.push(buffer[m - du]);
+                            faces.push(buffer[m - du - dv]);
+                            faces.push(buffer[m]);
+                            faces.push(buffer[m - du - dv]);
+                            faces.push(buffer[m - dv]);
+                            /*
+                            indices[index    ] = face[0];
+                            indices[index + 1] = face[1];
+                            indices[index + 2] = face[2];
+                            indices[index + 3] = face[0];
+                            indices[index + 4] = face[2];
+                            indices[index + 5] = face[3];
+                            */
                         } else {
-                            faces.push([buffer[m], buffer[m - dv], buffer[m - du - dv], buffer[m - du]]);
+                            //faces.push([buffer[m], buffer[m - dv], buffer[m - du - dv], buffer[m - du]]);
+                            faces.push(buffer[m]);
+                            faces.push(buffer[m - dv]);
+                            faces.push(buffer[m - du - dv]);
+                            faces.push(buffer[m]);
+                            faces.push(buffer[m - du - dv]);
+                            faces.push(buffer[m - du]);
                         }
                     }
                 }
         }
-    
+        let fx: f32 = (f32)(maxx / 2);
+        let fy: f32 = (f32)(maxx / 2);
+        let fz: f32 = (f32)(maxx / 2);
+        for (let i = 0; i < vertices.length; i += 3) {
+
+
+            vertices[i] -= fx;
+            vertices[i + 1] -= fy;
+            vertices[i + 2] -= fz;
+        }
         // All done!  Return the result
         return new SurfaceNet(StaticArray.fromArray(vertices), StaticArray.fromArray(faces));
     }
